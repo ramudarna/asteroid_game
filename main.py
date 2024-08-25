2c5dee6f2bfb5ea4b1f88c96bb1fe9fa29751081
ramudarna <ramudarna007@gmail.com>
2024-08-25 19:20:20 +0530

    Added delta time parameter

--- main.py
+++ main.py
@@ -3,16 +3,16 @@ import pygame
 
 def main():
     pygame.init()
-    print("Starting asteroids!")
-    print(SCREEN_WIDTH)
-    print(SCREEN_HEIGHT)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
         pygame.display.flip()
+        dt =  clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
