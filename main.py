6d9a586bc22dca21718bdfc8cd7f6076c1ecbd97
ramudarna <ramudarna007@gmail.com>
2024-08-29 21:04:13 +0530

    Added Player Class

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from constants import *
+from player import Player
 import pygame
 
 def main():
@@ -6,13 +7,16 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
         dt =  clock.tick(60)/1000
+
 
 if __name__ == "__main__":
     main()
