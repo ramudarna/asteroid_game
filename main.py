33dedca68664edf84ab1199979555128bfa3f65d
ramudarna <ramudarna007@gmail.com>
2024-09-04 21:19:59 +0530

    added Player instances to two groups

--- main.py
+++ main.py
@@ -4,7 +4,10 @@ import pygame
 
 def main():
     pygame.init()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Player.containers = (updatable, drawable)
     clock = pygame.time.Clock()
     dt = 0
     player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
@@ -12,9 +15,13 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        player.update(dt)
+            
+        for obj in updatable:
+            obj.update(dt)        
         screen.fill("black")
-        player.draw(screen)
+        for obj in drawable:
+            obj.draw(screen)
+            
         pygame.display.flip()
         dt =  clock.tick(60)/1000
 
